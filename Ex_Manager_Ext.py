8299adf5b0a065b58d5e432d14f64d59111fffc5
Thinh Doan <thinhdoan2608@gmail.com>
2026-03-07 18:09:22 +0700

    2

--- Ex_Manager_Ext.py
+++ Ex_Manager_Ext.py
@@ -29,7 +29,6 @@ class Add_Expense(QDialog):
         else:
             QMessageBox.warning(self, "Error", message)
 
-
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent= None):
         super().__init__(parent)
