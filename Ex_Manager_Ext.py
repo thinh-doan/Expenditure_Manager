03b8176ac35ae191c9d93ecba95784a57b835d29
Thinh Doan <thinhdoan2608@gmail.com>
2026-03-07 18:09:39 +0700

    2

--- Ex_Manager_Ext.py
+++ Ex_Manager_Ext.py
@@ -4,42 +4,54 @@ from PyQt6.QtWidgets import QMainWindow, QDialog, QMessageBox, QTableWidgetItem
 
 from Ex_Manager_Process import Ex_Manager_Process
 from Inter_MainWindow import Ui_MainWindow
-from Inter_Expense import Ui_MainWindow as ExpenseUI
-from Inter_Income import Ui_MainWindow as IncomeUI
-from Inter_Saving import Ui_MainWindow as SavingUI
+from Inter_Expense import Ui_Dialog as ExpenseUI
 
 class Add_Expense(QDialog):
-    def __init__(self, process, parent= None):
+
+    def __init__(self, process, parent=None):
         super().__init__(parent)
+
         self.ui = ExpenseUI()
         self.ui.setupUi(self)
+
         self.process = process
+
         self.ui.btnOK.clicked.connect(self.add_expense)
         self.ui.btnCancel.clicked.connect(self.reject)
 
     def add_expense(self):
+
         tr_category = self.ui.cbbCategory.currentText()
         tr_amount = self.ui.txtAmount.text()
         tr_date = self.ui.txtDate.text()
         tr_note = self.ui.txtNote.text()
 
         success, message = self.process.add_transaction("Expense", tr_category, tr_amount, tr_date, tr_note)
-        if success is True:
+
+        if success:
             self.accept()
         else:
             QMessageBox.warning(self, "Error", message)
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent= None):
+
+    def __init__(self, parent=None):
         super().__init__(parent)
+
         self.setupUi(self)
+
         self.process = Ex_Manager_Process()
 
-        #kết nối với nút bấm
+        # kết nối nút
         self.btnAdd_Expense.clicked.connect(self.open_add_expense)
 
+        # hiển thị dữ liệu ban đầu
+        self.hien_thi_tableInfor()
+
     def open_add_expense(self):
+
         dialog = Add_Expense(self.process, self)
+
         if dialog.exec():
             self.hien_thi_tableInfor()
 
--- Ex_Manager_Process.py
+++ Ex_Manager_Process.py
@@ -1,45 +1,45 @@
-# danh sách chi tiêu, thêm, xoá, tính tổng tiền, lưu file
-from datetime import datetime, timedelta
+from datetime import datetime
+
 
 class Ex_Manager_Process:
+
     ds = []
-    
+
     def add_transaction(self, tr_type, tr_category, tr_amount, tr_date, tr_note):
+
         try:
             tr_amount = float(tr_amount)
+
             if tr_amount <= 0:
                 return False, "Số tiền phải là số dương!"
-            
-            try: # why try not if?
-                # Validate date format
+
+            # kiểm tra định dạng ngày
+            try:
                 datetime.strptime(tr_date, "%d/%m/%Y")
             except ValueError:
-                return False, "Định dạng ngày không hợp lệ, nhập lại DD/MM/YYYY!"
+                return False, "Định dạng ngày không hợp lệ! (DD/MM/YYYY)"
 
             transaction = {
-                'date': tr_date,
-                'type': tr_type,
-                'category': tr_category,
-                'amount': tr_amount,
-                'note': tr_note
+                "date": tr_date,
+                "type": tr_type,
+                "category": tr_category,
+                "amount": tr_amount,
+                "note": tr_note
             }
-            Ex_Manager_Process.ds.append(transaction)
-            return True, "Transaction added successfully"
-        
-        except ValueError:
-            return False, "Invalid amount"
 
-    @classmethod    
+            Ex_Manager_Process.ds.append(transaction)
+
+            return True, "Transaction added successfully"
+
+        except ValueError:
+            return False, "Số tiền không hợp lệ!"
+
+    @classmethod
     def get_transactions(cls):
-        return cls.ds
+        return cls.ds.copy()
 
     def luu_tru(self):
         pass
 
     def hien_thi(self):
         pass
-
-
-
-
-         
